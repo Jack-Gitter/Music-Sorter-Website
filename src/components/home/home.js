85df6a8e6164adfb33290c9eb0eb8e5bd196e119
Jack Gitter <gitter.j@husky.neu.edu>
2023-03-30 18:12:33 -0400

    nice

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,8 +1,11 @@
+import { useParams, useSearchParams } from "react-router-dom";
 import HeaderComponent from "./header";
 import PlaylistSelector from "./playlist-selector";
 import Sliders from "./sliders";
 
 const Home = () => {
+    const [searchParams] = useSearchParams(); 
+    const code = searchParams.get('code');
     return (
         <>
         <HeaderComponent/>
