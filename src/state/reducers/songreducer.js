0b379a5458933341751e61d5817af3b0d4af4d49
Jack Gitter <gitter.j@husky.neu.edu>
2023-03-31 17:25:34 -0400

    cool

--- src/state/reducers/songreducer.js
+++ src/state/reducers/songreducer.js
@@ -48,7 +48,6 @@ const userInfo = createSlice({
                 state.songs.sort((song1, song2) => song1.curated_value - song2.curated_value)
                 console.log('this is the state \'s songs!')
                 console.log(state.songs);
-                console.log(state.songs[0].curated_value)
             }
     },
 })
