0ddd97fb1d8ae5de993a891346d4553ad31a3876
Jack Gitter <gitter.j@husky.neu.edu>
2023-03-31 11:26:19 -0400

    done

--- src/components/home/currentsong.js
+++ src/components/home/currentsong.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 const CurrentSong = () => {
+
+    const {playlists, songs, sliders} = useSelector((store) => store.userInfoReducer);
+    const dispatcher = useDispatch();
+
     return (
         <>
-        <h1>Current song is still woooozy</h1>
+        <h1>Current song is {songs[0]}</h1>
         </>
     );
 }
