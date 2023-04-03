890b5a74a609355dbafcbbf6247a8dcaf9c8f77e
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-03 17:39:33 -0400

    nice

--- src/components/home/webplayback.js
+++ src/components/home/webplayback.js
@@ -6,13 +6,22 @@ const WebPlayback = () => {
     const {currentPlaylist, loadingSongs, accessToken, refreshToken, playlists, songs, sliders} = useSelector((store) => store.userInfoReducer);
 
     const songUris = []
-    for (let i = 0; i < songs.length; i++) {
+    for (let i = 0; i < songs.length && i < 774; i++) {
         songUris.push(songs[i].uri);
     }
     
     return (
         <>
         <SpotifyPlayer 
+        styles={{
+            activeColor: '#fff',
+            bgColor: '#333',
+            color: '#fff',
+            loaderColor: '#fff',
+            sliderColor: '#1cb954',
+            trackArtistColor: '#ccc',
+            trackNameColor: '#fff',
+          }}
         token={accessToken}
         layout={'responsive'}
         play={true}
