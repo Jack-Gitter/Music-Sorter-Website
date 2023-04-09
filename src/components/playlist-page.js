e8837b88471eb7526deebaabc9677c490fa2ae4e
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-09 17:55:14 -0400

    nice

--- src/components/playlist-page.js
+++ src/components/playlist-page.js
@@ -35,7 +35,7 @@ const PlaylistPage = () => {
         dispatcher(setCurrentPlaylist(plistID));
         dispatcher(setLoadingMetrics(true));
         dispatcher(getBoundedVariablesThunk(plistID));
-        if (playerState.nextTracks !== undefined) {
+        if (playerState.nextTracks !== undefined && playerState.nextTracks.length > 0) {
         let trackID = playerState.nextTracks[0].id
             for (let i = 0; i < songs.length; i++) {
                 console.log(songs[i].id)
