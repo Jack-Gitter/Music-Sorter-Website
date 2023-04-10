fcbed5d8fab3189a8435dded64c68c4cd5536628
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-10 08:42:43 -0400

    somethign

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -8,8 +8,8 @@ const Search = () => {
     const dispatcher = useDispatch();
     const [searchQuery, updateSearchQuery] = useState('')
     return (
-        <div className="">
-            <input className='border border-secondary rounded'value={searchQuery} onChange={(e) => updateSearchQuery(e.target.value)}></input>
+        <div className="form-group">
+            <input className='left-shift p-1 mb-1 form-control border border-secondary rounded'value={searchQuery} onChange={(e) => updateSearchQuery(e.target.value)}></input>
             <button className='btn btn-success ms-2' onClick={() => {
                 dispatcher(searchSpotifyThunk(searchQuery));
             }}
