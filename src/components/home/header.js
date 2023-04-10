b91b0ee1b7376cb38452e54adc74faec71dfd53d
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-10 08:53:31 -0400

    dam

--- src/components/home/header.js
+++ src/components/home/header.js
@@ -6,24 +6,20 @@ import Search from "../search/search";
 const HeaderComponent = () => {
     const {accessToken, refreshToken, playlists, songs, sliders} = useSelector((store) => store.userInfoReducer);
     return (
-        <>
-            <div className="row">
+    <>
+        <div className="row">
                 <div className="col-4"></div>
-                <div className="col-5">
-                    <h1 className="fg-green">
-                        Curated Shuffle
-                    </h1>
+                <div className="col-4">
+                    <h1 className="fg-green">Curated Shuffle</h1>
                 </div>
-                <div className="col-3"></div>
-            </div>
-            <div className="row">
                 <div className="col-4"></div>
-                <div className="col-6">
-                    <Search/>
-                </div>
-                <div className="col-2"></div>
-            </div>
-        </>
+        </div>
+        <div className="row">
+            <div className="col-4"></div>
+            <div className="col-5"><Search/></div>
+            <div className="col-3"></div>
+        </div>
+    </>
     );
 }
 export default HeaderComponent;
