7dd40dd54d347d9a61c3fac69cd67c7dbc4044d3
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-11 18:22:50 -0400

    someone

--- src/components/home/playlist-selector.js
+++ src/components/home/playlist-selector.js
@@ -25,12 +25,12 @@ const PlaylistSelector = () => {
         <div className="container-fluid">
         <div className="row">
         {playlists.map((plist, index) => 
-            <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
-            <Link to={`/playlist/${getPlaylistID(plist.name)}/access_token=${accessToken}`}>
-                <img className={`playlist-img`} src={`
-                    ${ plist.images[0] === undefined ? 'https://picsum.photos/200/300' : plist.images[0].url}
-                `}>
-                </img>
+            <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2 pt-2">
+                <Link to={`/playlist/${getPlaylistID(plist.name)}/access_token=${accessToken}`}>
+                    <img className={`playlist-img`} src={`
+                        ${ plist.images[0] === undefined ? 'https://picsum.photos/200/300' : plist.images[0].url}
+                    `}>
+                    </img>
                 </Link>
             </div>
         )}
