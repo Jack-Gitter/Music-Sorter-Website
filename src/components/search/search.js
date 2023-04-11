2c7ea38c99703e87a20c4ffd138a4f94e7c585e3
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-10 22:35:39 -0400

    decent

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -8,12 +8,12 @@ const Search = () => {
     const dispatcher = useDispatch();
     const [searchQuery, updateSearchQuery] = useState('')
     return (
-        <div className="form-group">
-            <input className='ps-3 pt-1 pb-1 mb-2 form-control border border-secondary rounded-pill'value={searchQuery} onChange={(e) => updateSearchQuery(e.target.value)}></input>
-            <div className="search-buttons">
-            <button className='btn btn-large btn-dark m-2' onClick={() => {dispatcher(searchSpotifyThunk(searchQuery));}}>Search</button>
-            <button className='btn btn-large btn-dark m-2'onClick={() => {dispatcher(getUserPlaylistsThunk());}}>Reload my playlists!</button>
-            </div>
+        <div className="input-group">
+                <input type="text" className='form-control border rounded-pill'value={searchQuery} onChange={(e) => updateSearchQuery(e.target.value)}></input>
+                <div className="search-buttons">
+                    <button className='btn btn-large btn-dark m-2' onClick={() => {dispatcher(searchSpotifyThunk(searchQuery));}}>Search</button>
+                    <button className='btn btn-large btn-dark m-2'onClick={() => {dispatcher(getUserPlaylistsThunk());}}>Reload my playlists!</button>
+                </div>
         </div>
     );
 }
