dfdfd29109ce4827e5c918f30741f11eb665daf0
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-11 18:55:37 -0400

    try

--- src/components/home/playlist-selector.js
+++ src/components/home/playlist-selector.js
@@ -28,7 +28,7 @@ const PlaylistSelector = () => {
             <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2 pt-2">
 
                 <Link className="link-tag" to={`/playlist/${getPlaylistID(plist.name)}/access_token=${accessToken}`}>
-                        <span className="fg-white album-title">{plist.name}</span>
+                        <span className="fg-white album-title align-middle">{plist.name}</span>
                         <img className={`playlist-img`} src={`
                             ${ plist.images[0] === undefined ? 'https://picsum.photos/200/300' : plist.images[0].url}
                         `}>
