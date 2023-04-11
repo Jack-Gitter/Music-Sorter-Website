82e227fec9831ae3655d22e365161264d3b82a3d
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-11 18:49:11 -0400

    eh

--- src/components/home/playlist-selector.js
+++ src/components/home/playlist-selector.js
@@ -27,7 +27,7 @@ const PlaylistSelector = () => {
         {playlists.map((plist, index) => 
             <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2 pt-2">
 
-                <Link to={`/playlist/${getPlaylistID(plist.name)}/access_token=${accessToken}`}>
+                <Link className="link-tag" to={`/playlist/${getPlaylistID(plist.name)}/access_token=${accessToken}`}>
                         <span className="fg-white album-title">{plist.name}</span>
                         <img className={`playlist-img`} src={`
                             ${ plist.images[0] === undefined ? 'https://picsum.photos/200/300' : plist.images[0].url}
