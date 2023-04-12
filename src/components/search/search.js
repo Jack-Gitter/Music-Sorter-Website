6a8b8aa4c993a856fcdf7a83d86290a528aa3bc0
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-11 22:31:03 -0400

    yeah

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -9,10 +9,10 @@ const Search = () => {
     const [searchQuery, updateSearchQuery] = useState('')
     return (
         <>
-            <div className="col-7 pt-2">
+            <div className="col-6 pt-2">
                     <input type="text" className='ps-3 pt-1 pb-1 mb-2 form-control border border-secondary rounded-pill'value={searchQuery} onChange={(e) => updateSearchQuery(e.target.value)}></input>
             </div>
-            <div className="col-3">
+            <div className="col-4">
                 <button className='btn btn-large btn-dark m-2' onClick={() => {dispatcher(searchSpotifyThunk(searchQuery));}}>Search</button>
                 <button className='btn btn-large btn-dark m-2'onClick={() => {dispatcher(getUserPlaylistsThunk());}}>My Playlists</button>
             </div>
