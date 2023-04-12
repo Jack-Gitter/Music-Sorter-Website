15666463bac140c10fea9c355549c032c7790f38
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-11 23:09:42 -0400

    lol

--- src/components/home/playlist-selector.js
+++ src/components/home/playlist-selector.js
@@ -23,7 +23,7 @@ const PlaylistSelector = () => {
 
     return (
         <div>
-        <div className="row ms-2 me-2">
+        <div className="row ps-2 pe-2">
         {playlists.map((plist, index) => 
             <div className="col-sm-6 position-relative col-md-4 col-lg-3 pt-2">
                 <span className="fg-white m-2 position-absolute album-title">{plist.name}</span>
