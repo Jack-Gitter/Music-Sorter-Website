3712663e6f986d35dab6b7f72ba01ab2095b4158
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-13 17:25:59 -0400

    someone

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -19,10 +19,10 @@ const Search = () => {
                 </button>
             </div>
             </div>
-            <div className="col-sm-2 col-md-1 col-1 text-center">
+            <div className="col-sm-2 col-md-1 col-1">
                 <button className='btn btn-large btn-dark m-2'onClick={() => {dispatcher(getUserPlaylistsThunk());}}>Reload</button>
             </div>
-            <div className="col-sm-2 col-md-1 col-1 text-center">
+            <div className="col-sm-2 col-md-1 col-1">
                 <Link to={'/'}><button className={'btn btn-dark m-2'}>Logout</button></Link>
             </div>
         </>
