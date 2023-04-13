97202bf32e51282e5d1e2185ba0f6736d9b4f37e
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-13 10:47:33 -0400

    why

--- src/components/playlist/playlist-page.js
+++ src/components/playlist/playlist-page.js
@@ -36,7 +36,7 @@ const PlaylistPage = () => {
     return (
         <>
         <div className="row position-relative m-0 p-0">
-                {(playlistIMG !== 'none' && playlistIMG !== '') && <span className="position-absolute album-title-big">{plistName}</span>}
+                {(playlistIMG !== 'none' && playlistIMG !== '') && <span className="position-relative album-title-big">{plistName}</span>}
                 {(playlistIMG !== 'none' && playlistIMG !== '') && <img className="playlist-img-big" src={playlistIMG}></img>}
         </div>
         <div className="row m-0 p-0">
