803440343ff63a9018c8f57ca8627bd6c62e2c64
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-13 22:30:22 -0400

    huh

--- src/components/home/sliders.js
+++ src/components/home/sliders.js
@@ -39,8 +39,8 @@ const Sliders = () => {
     const [loudnessEnabled, loudnessStatus] = useState(true)
 
     return (
-        <div className="row d-flex align-items-center justify-content-center">
-        <div className="col-sm-6 col-md-4 col-lg-3 m-1 p-1"> 
+        <div className="row">
+        <div className="col-sm-6 col-md-4 col-lg-3 m-0 p-0"> 
                 <div className="row text-center">
                     <label className="form-label" for="acousticness-range">
                         <span className="slider-text-color me-2 ">acousticness</span>
