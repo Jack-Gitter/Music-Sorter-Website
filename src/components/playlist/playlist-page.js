705b6a1e6c880cce23ac96f984c5b54d073ea83c
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-18 17:06:00 -0400

    try

--- src/components/playlist/playlist-page.js
+++ src/components/playlist/playlist-page.js
@@ -44,7 +44,7 @@ const PlaylistPage = () => {
             <div className="row position-absolute next-songs">
                 <ul cassName="">
                     {songs.slice(firstSongDisplayedIDX,firstSongDisplayedIDX+5).map((track) => 
-                        <li className="">
+                        <li className="fg-white">
                             {track.name}
                         </li>
                     )}
