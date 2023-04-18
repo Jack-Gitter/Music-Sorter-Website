c484f4a9c74ff0a9b5d1c7d2e9a6bb1a1d6a7cb2
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-17 22:24:49 -0400

    relocated

--- src/components/playlist/playlist-page.js
+++ src/components/playlist/playlist-page.js
@@ -40,14 +40,14 @@ const PlaylistPage = () => {
             <div className="row position-relative">
                     {(playlistIMG !== 'none' && playlistIMG !== '') && <span className="position-absolute album-title-big">{plistName}</span>}
                     {(playlistIMG !== 'none' && playlistIMG !== '') && <img className="playlist-img-big" src={playlistIMG}></img>}
+                    <ul cassName="position-absolute next-songs">
+                        {songs.slice(firstSongDisplayedIDX,firstSongDisplayedIDX+5).map((track) => 
+                            <li className="">
+                                {track.name}
+                            </li>
+                        )}
+                    </ul>
             </div>
-            <ul cassName="position-absolute next-songs">
-                {songs.slice(firstSongDisplayedIDX,firstSongDisplayedIDX+5).map((track) => 
-                    <li className="">
-                        {track.name}
-                    </li>
-                )}
-            </ul>
             {/*<div className="row">
                 <ul cassName="list-group">
                     {songs.slice(firstSongDisplayedIDX,firstSongDisplayedIDX+5).map((track) => 
