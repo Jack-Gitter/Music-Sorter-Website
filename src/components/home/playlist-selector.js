afb5d213bdeca37c20764b624195542dd17e94c1
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-17 21:21:38 -0400

    why not?

--- src/components/home/playlist-selector.js
+++ src/components/home/playlist-selector.js
@@ -35,7 +35,7 @@ const PlaylistSelector = () => {
     return (
         <div className="row">
         {playlists.map((plist, index) => 
-            <div className="col-sm-6 col-md-4 col-lg-3 position-relative increase-size justify-content-center">
+            <div className="col-sm-6 col-md-4 col-lg-3 position-relative increase-size d-flex align-items-center">
                 <Link className="link-tag" 
                 to={`/playlist/${getPlaylistID(plist.name)}/${plist.name}/access_token=${accessToken}`}>
                 <span className="fg-white position-absolute album-title">{plist.name}</span>
