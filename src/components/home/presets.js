11f387cb9bd358e37aaccf0de75774faff6db188
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-23 19:38:03 -0400

    nah

--- src/components/home/presets.js
+++ src/components/home/presets.js
@@ -32,7 +32,7 @@ const Presets = () => {
         </div>
 
         <div className="col-2">
-        <button className="btn btn-info"
+        <button className="btn btn-info w-100"
         onClick={() => {
                 dispatcher(updateSliderEnabled({...defaultDisabled, tempo: true, energy: true, instrumentalness: true, loudness: true, speechiness: true, valence: true}));
                 dispatcher(updateSliders({...sliders, tempo: 30, energy: 50, instrumentalness:60, loudness:30, speechiness: 35, valence: 50}));
@@ -43,7 +43,7 @@ const Presets = () => {
         </div>
 
         <div className="col-2">
-        <button className="btn btn-danger"
+        <button className="btn btn-danger w-100"
         onClick={() => {
             dispatcher(updateSliderEnabled({...defaultDisabled, danceability: true, energy: true, loudness: true, tempo: true}));
             dispatcher(updateSliders({...sliders, danceability: 80, energy: 100, loudness: 85, tempo: 90}));
@@ -54,7 +54,7 @@ const Presets = () => {
         </div>
 
         <div className="col-2">
-        <button className="btn btn-warning"
+        <button className="btn btn-warning w-100"
         onClick={() => {
             dispatcher(updateSliderEnabled({...defaultDisabled, danceability: true, duration_ms: true, energy: true, instrumentalness: true, liveness: true, loudness: true, tempo: true, valence: true}));
             dispatcher(updateSliders({...sliders, danceability: 100, duration_ms: 40, energy: 100, instrumentalness: 20, liveness: 0, loudness: 70, tempo: 70, valence: 80}));
@@ -65,7 +65,7 @@ const Presets = () => {
         </div>
         
         <div className="col-2">
-        <button className="btn btn-secondary"
+        <button className="btn btn-secondary w-100"
         onClick={() => {
             dispatcher(updateSliderEnabled({...defaultDisabled, acousticness: true, energy: true, valence: true, tempo: true, loudness: true}));
             dispatcher(updateSliders({...sliders, acousticness: 50, energy: 25, valence: 40, tempo: 30, loudness: 30}));
