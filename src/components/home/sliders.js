15d5b56cbd9e92c934a3f24b09a07e606fca746a
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-24 22:51:15 -0400

    pls work

--- src/components/home/sliders.js
+++ src/components/home/sliders.js
@@ -33,6 +33,7 @@ const Sliders = () => {
         </div>
         <div className="col-sm-6 col-md-4 col-lg-3 m-0 p-0">
             <div className="row text-center">
+                <span class="form-check form-switch m-0 p-0">
                 <label for="danceability-range form-check form-switch">
                         <span className="slider-text-color me-2 ">danceability</span>
                         <span className="slider-text-color ">{sliders.danceability}</span>
@@ -40,6 +41,7 @@ const Sliders = () => {
             {/*sliderEnabled.danceability && <button className={'btn btn-dark ms-2'} onClick={() => dispatcher(updateSliderEnabled({...sliderEnabled, danceability: false}))}>Disable</button>}
             {!sliderEnabled.danceability && <button className={'btn btn-secondary ms-2'} onClick={() => dispatcher(updateSliderEnabled({...sliderEnabled, danceability: true}))}>Enable</button>*/}
                 </label>
+                </span>
             </div>
             <div className="row">
                 <input id="danceability-range" className="form-range p-4 " disabled={!sliderEnabled.danceability} onChange={(e) => dispatcher(updateSliders({...sliders, danceability: e.target.value}))} type='range' min='0' max='100' value={sliders.danceability}></input>
