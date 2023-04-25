8dc91b5aa979e98de508deafe09fb1f2e697e04f
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-24 22:04:31 -0400

    ok

--- src/components/home/webplayback.js
+++ src/components/home/webplayback.js
@@ -10,7 +10,7 @@ const WebPlayback = () => {
     const {currentPlaylist, loadingSongs, songUris, accessToken, refreshToken, playlists, songs, sliders} = useSelector((store) => store.userInfoReducer);
 
     return (
-        <div className="row ms-3 me-3">
+        <div className="row ms-3 me-3 position-relative wp">
         {songUris.length > 0 && <SpotifyPlayer 
         styles={{
             activeColor: '#fff',
