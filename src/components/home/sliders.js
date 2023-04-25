cf4b549c0620938dfac57b551d1b1d638223bd3d
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-24 22:43:32 -0400

    testin

--- src/components/home/sliders.js
+++ src/components/home/sliders.js
@@ -34,9 +34,9 @@ const Sliders = () => {
         <div className="col-sm-6 col-md-4 col-lg-3 m-0 p-0">
             <div className="row text-center">
                 <label for="danceability-range">
-                    <span className="slider-text-color me-2 ">danceability</span>
-                    <span className="slider-text-color ">{sliders.danceability}</span>
                     <span class="form-check form-switch">
+                        <span className="slider-text-color me-2 ">danceability</span>
+                        <span className="slider-text-color ">{sliders.danceability}</span>
                         <input onClick={() => dispatcher(updateSliderEnabled({...sliderEnabled, danceability: !sliderEnabled.danceability}))}  class="form-check-input" type="checkbox" role="switch" checked={sliderEnabled.danceability}/>
                     </span>
             {/*sliderEnabled.danceability && <button className={'btn btn-dark ms-2'} onClick={() => dispatcher(updateSliderEnabled({...sliderEnabled, danceability: false}))}>Disable</button>}
