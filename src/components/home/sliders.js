4e4b6a3476453aa655ded2f7744efe4d59e65257
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-25 18:52:34 -0400

    no com

--- src/components/home/sliders.js
+++ src/components/home/sliders.js
@@ -25,7 +25,7 @@ const Sliders = () => {
                         <span className="slider-text-color me-2 ">acousticness</span>
                         {sliderEnabled.acousticness && <span className="slider-text-color ">{sliders.acousticness}</span>}
                         <input onClick={() => dispatcher(updateSliderEnabled({...sliderEnabled, acousticness: !sliderEnabled.acousticness}))}  class="form-check-input " type="checkbox" role="switch" checked={sliderEnabled.acousticness}/>
-                        <i class="fa fas fa-info-circle fg-black"></i>
+                        <i class="fa fas fa-info-circle fg-white"></i>
                     </label>
                 </span>
                 </div>
