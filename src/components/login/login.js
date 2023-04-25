4fcf2660445c0f1fb3e16ef328b404bb6bbc887c
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-25 10:44:54 -0400

    workin

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,7 +2,7 @@ const Login = () => {
     return (
         <div className="login-page">
             <a href={'https://curatedshuffleauthserver.onrender.com/login'}>
-                <button className={'btn btn-lg btn-dark'}>
+                <button className={'btn btn-lg btn-dark btn-opacity'}>
                     <i className="fa fas fa-2x fa-brands fa-spotify pt-1"></i>
                     <div className="ps-4 pe-4 pb-1">Login with spotify</div>
                 </button>
